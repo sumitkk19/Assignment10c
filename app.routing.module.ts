5900c1fffbe27a8f914380173717bd6696381814
sumitkk19 <65333951+sumitkk19@users.noreply.github.com>
2023-04-22 18:33:53 +0530

    Add files via upload

---
+++ app.routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule} from "@angular/router";
+import { TechnologiesComponent } from "./technologies/technologies.component";
+import { BooksComponent } from "./books/books.component";
+import { WrongChoiceComponent } from "./wrong-choice/wrong-choice.component";
+
+
+ 
+const routes: Routes = [
+    { path: 'technologies', component: TechnologiesComponent },
+    { path: 'books', component: BooksComponent },
+
+    { path: '', component: TechnologiesComponent },
+    
+    { path: '**', component:WrongChoiceComponent }
+
+    
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ wrong-choice.component.ts
@@ -0,0 +1,12 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-invalid-page',
+    templateUrl: './wrong-choice-component.html',
+})
+export class WrongChoiceComponent implements OnInit {
+    constructor() { }
+    ngOnInit() {
+    }
+}
